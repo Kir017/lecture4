a76db68935c41020974c3fd455aff5ae987918a4
Kirill Kovalev <kir017@mail.ru>
2023-01-13 19:02:08 +0300

    Код по 4 лекции курса С#

---
+++ task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Метод поиска факториала числа
+
+double Factorial (int n)
+{
+    //1! = 1
+    //0! = 1
+    if(n == 1) return 1;
+    else return n * Factorial(n -1);
+}
+
+// Console.WriteLine(Factorial(3)); // 1*2*3 = 6
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}"); 
+}
+
